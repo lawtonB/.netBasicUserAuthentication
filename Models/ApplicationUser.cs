95e7729372ee99246f5c6e36fbd5fbf83f7773c4
Simon Lawton <simonctemple@live.com lawton.a.browning@gmail.com>
2016-04-25 11:38:29 -0700

    integrate application data with identity for item

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace BasicAuthentication.Models
 {
